4dbdc0626fa4a75b3ca59d1ed08679106197800b
acidandsalt <seawindskyforest@outlook.com>
2023-01-24 12:07:24 +0800

    Site updated: 2023-01-24 12:07:23

--- javascripts/FunnyTitle.js
+++ javascripts/FunnyTitle.js
@@ -1,14 +1,13 @@
-<!--浏览器搞笑标题-->
- var OriginTitle = document.title;
+var OriginTitle = document.title;
  var titleTime;
  document.addEventListener('visibilitychange', function () {
      if (document.hidden) {
-         $('[rel="icon"]').attr('href', "/img/trhx2.png");
+         $('[rel="icon"]').attr('href', "/out.png");
          document.title = '别担心，在这里等你回来:)';
          clearTimeout(titleTime);
      }
      else {
-         $('[rel="icon"]').attr('href', "/img/trhx2.png");
+         $('[rel="icon"]').attr('href', "/back.png");
          document.title = 'Hi，欢迎回来！' + OriginTitle;
          titleTime = setTimeout(function () {
              document.title = OriginTitle;
